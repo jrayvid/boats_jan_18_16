c8711faaad367dbd8b1916dffa28e84b4c11305a
jon <jrayvid@gmail.com>
2016-01-18 15:58:04 -0500

    added signin pages and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   
+  get 'sessions/new'
 
-  get 'users/new'
+get '/login' => 'sessions#new'
+post '/login' => 'sessions#create'
 
-  get 'static_pages/about'
+  
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
